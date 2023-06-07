6c32b098ae2995d9ed022583048027e423ec8c24
Alimgahed <100706648+Alimgahed@users.noreply.github.com>
2023-06-07 15:45:20 +0300

    Update ali mohamed.R

--- ali mohamed.R
+++ ali mohamed.R
@@ -1,27 +1,26 @@
-# Read the CSV file
-df <- read.csv("G3_sydney_hobart_times.csv", header = TRUE)
-
+# Read the  file
+ds <- read.csv("G3_sydney_hobart_times.csv", header = TRUE)
 # Remove "day" from the Time column and convert it to numeric
-df$Time <- as.numeric(sub("day", "", df$Time))
+ds$Time <- as.numeric(sub("day", "", ds$Time))
 
-# Set a single value for multiple rows at once
+# get a mean of time to input the missing value 
 mean_time <- mean(df$Time)
 cat("Mean Time:", mean_time, "\n")
 
 rows_to_update <- c(5, 9, 13, 19, 31, 46, 58)
-df[rows_to_update, "Time"] <- 6.959848
+ds[rows_to_update, "Time"] <- 6.959848
 
-# Remove the Code.Time.less.than.3 column
-df <- subset(df, select = -Code.Time.less.than.3)
+# Remove the Code.Time.less.than.3 column becuse all values is nall
+ds <- subset(ds, select = -Code.Time.less.than.3)
 
 # Check the structure of the dataframe
-str(df)
+str(ds)
 
 # Calculate the minimum and maximum time
-min_time <- min(df$Time)
-max_time <- max(df$Time)
+min_time <- min(ds$Time)
+max_time <- max(ds$Time)
 cat("Minimum Time:", min_time, "\n")
 cat("Maximum Time:", max_time, "\n")
 
 # Create a histogram of Time
-hist(df$Time, breaks = 20, xlab = "Time", ylab = "Frequency", main = "Distribution of Recorded Times")
+hist(ds$Time, breaks = 20, xlab = "Time", ylab = "Frequency", main = "Distribution of Recorded Times")
