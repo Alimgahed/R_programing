216c007405218b8b4084c7ff7a8476c2a805e81a
Alimgahed <100706648+Alimgahed@users.noreply.github.com>
2023-06-09 16:14:42 +0300

    Update ali mohamed.R

--- ali mohamed.R
+++ ali mohamed.R
@@ -2,7 +2,6 @@
 ds <- read.csv("G3_sydney_hobart_times.csv", header = TRUE)
 # Remove "day" from the Time column and convert it to numeric
 ds$Time <- as.numeric(sub("day", "", ds$Time))
-
 # get a mean of time to input the missing value 
 mean_time <- mean(df$Time)
 cat("Mean Time:", mean_time, "\n")
@@ -14,35 +13,29 @@ ds[rows_to_update, "Code.Time.less.than.3"] <- ifelse(ds$Time < 3, "less than 3"
 ds
 # Check the structure of the dataframe
 str(ds)
-
 # Calculate the minimum and maximum time
 min_time <- min(ds$Time)
 max_time <- max(ds$Time)
 cat("Minimum Time:", min_time, "\n")
 cat("Maximum Time:", max_time, "\n")
 library(ggplot2)
-
 # Create the bar chart of code.time.less.than.3
 ggplot(ds, aes(x = Year, fill = Code.Time.less.than.3)) +
   geom_bar() +
   labs(x = "Year", y = "Count", fill = "Code Time") +
   scale_fill_manual(values = c("more than 3" = "black", "less than 3" = "grey")) +
   theme_minimal()
-library(ggplot2)
-
 # Create scatter plot of fleet start and fleet finish 
 ggplot(ds) +
-  geom_point(aes(x = fleet_start, y = fleet_finish), color = "blue", size = 3) +
+  geom_point(aes(x =Year, y = Time, color = Code.Time.less.than.3), size = 3) +
   labs(title = "Start Fleet vs Finish Fleet",
        x = "Fleet Start",
        y = "Fleet Finish") +
-  theme_minimal()
-# Create a histogram of year
-ggplot(ds) +
-  geom_histogram(aes(x = Year), binwidth = 1, fill = "lightblue", color = "black") +
-  labs(title = "Histogram of Year",
-       x = "Year",
-       y = "Frequency") +
-  theme_minimal()
+  theme_minimal()
+# Create the histogram OF YEAR AND TIME
+ggplot(data = ds, aes(x = Year, y = Time)) +
+  geom_col(fill = "blue") +
+  labs(x = "Year", y = "Time") +
+  ggtitle("Histogram of Year vs. Time")
 # Create a histogram of Time
 hist(ds$Time, breaks = 20, xlab = "Time", ylab = "Frequency", main = "Distribution of Recorded Times")
